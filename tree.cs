3f189baa463bf3a3f74a6cee09b9794292e4de39
Maria Lyashenko <mariia.liashenko@nure.ua>
2021-04-29 22:16:01 +0300

    create class node

--- tree.cs
+++ tree.cs
@@ -1,1 +1,23 @@
-# code
+using System;
+using System.Collections.Generic;
+
+namespace ATSD2lab
+{
+    class Program
+    { 
+      static void Main(string[] args)
+        {
+
+        }  
+    }
+}
+class Node<T>
+    {
+        public T data;
+        public Node<T> left;
+        public Node<T> right;
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
