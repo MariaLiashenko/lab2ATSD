0267cdd0e640b91982099e13410331271432ef05
Maria Lyashenko <mariia.liashenko@nure.ua>
2021-04-29 22:36:42 +0300

    Recursive add

--- tree.cs
+++ tree.cs
@@ -48,6 +48,27 @@ class Node<T>
                     root = RecursiveInsert(root, newItem);
                 }
             }
+            private Node<T> RecursiveInsert(Node<T> current, Node<T> n)
+            {
+                if (current == null)
+                {
+                    current = n;
+                    return current;
+                }
+                //<
+                else if (Comparer<T>.Default.Compare(current.data,n.data) == 1  )
+                {
+                    current.left = RecursiveInsert(current.left, n);
+                    current = balance_tree(current);
+                }
+                //>
+                else if (Comparer<T>.Default.Compare(n.data,current.data) == 1  )
+                {
+                    current.right = RecursiveInsert(current.right, n);
+                    current = balance_tree(current);
+                }
+                return current;
+            }
         }
     
     }
